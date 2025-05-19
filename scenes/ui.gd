14dc8136725010b8ca7705cc9b6bf9c0a8e8455b
P2660701 <P2660701@my365.dmu.ac.uk>
2025-05-19 17:14:23 +0100

    changes to UI and Autoloads
    Doesn't include changes made to the ore scenes and scripts. They were better off before

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -1,3 +1,7 @@
+## needs to be an overlay to the main level scene
+## controlling the incrementation when events occur with the player character body
+## signals may need to be transmitted between scens in that case
+
 extends Control
 
 var health = 100 # if you do it in a function block then it'll be only be availble in that scope of the script
