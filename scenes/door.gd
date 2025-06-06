0d58d6249821d65bce2dfdf4ee7ff80cee8347d0
P2660701 <P2660701@my365.dmu.ac.uk>
2025-06-06 07:26:47 +0100

    Final commit
    + Area added to the door to end the level
    + SPG corrections were made to the educational content scenes

--- scenes/door.gd
+++ scenes/door.gd
@@ -1,5 +1,7 @@
-extends TileMapLayer
+## transitions to the end scene
+
+extends Area2D
 
 
-func _on_area_2d_body_entered(body: Node2D):
+func _on_body_entered(body: CharacterBody2D):
 	SceneSwitcher.goto_scene("res://scenes/end.tscn")
